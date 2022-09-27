5a5956c1238429e196958c3f3c8018ea2d6c8783
Ewicks <ethanowicks9@gmail.com>
2022-09-27 17:49:00 +0000

    Add

--- run2.py
+++ run2.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 baseurl = 'https://emaps.elmbridge.gov.uk/ebc_planning.aspx'
 
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
-
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
 
 r = requests.get('https://emaps.elmbridge.gov.uk/ebc_planning.aspx?pageno=1&template=AdvancedSearchResultsTab.tmplt&requestType=parseTemplate&USRN%3APARAM=&apptype%3APARAM=&status%3APARAM=&decision%3APARAM=&ward%3APARAM=&txt_search%3APARAM=&daterec_from%3APARAM=2022-06-01&daterec_to%3APARAM=2022-09-30&datedec_from%3APARAM=&datedec_to%3APARAM=&pagerecs=50&orderxyz%3APARAM=REG_DATE_DT%3ADESCENDING&SearchType%3APARAM=Advanced', headers=headers)
 
@@ -14,10 +14,18 @@ houselist = soup.find_all('tr')
 
 linkslist = []
 
+updatehouselist = []
+
 for house in houselist:
+    if (house.find('td', string=re.compile('extension'))):
+        updatehouselist.append(house)
+
+
+for house in updatehouselist:
     for link in house.find_all('a', href=True):
         homepagelinks = link['href']
         linkslist.append(homepagelinks)
+
 
 contactlinkslist = []
 
@@ -36,7 +44,7 @@ for link in contactlinkslist:
     r = requests.get(link, headers=headers)
     soup = BeautifulSoup(r.content, 'lxml')
     atags = soup.find('div', class_='atPanelContainer').find('dl')
-    contactnameslist.append(atags.get_text())
+    contactnameslist.append(atags)
 
     
 print(contactnameslist)
--- test.py
+++ test.py
@@ -9,10 +9,6 @@ headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Geck
 
 r = requests.get('https://emaps.elmbridge.gov.uk/ebc_planning.aspx?pageno=1&template=AdvancedSearchResultsTab.tmplt&requestType=parseTemplate&USRN%3APARAM=&apptype%3APARAM=&status%3APARAM=&decision%3APARAM=&ward%3APARAM=&txt_search%3APARAM=&daterec_from%3APARAM=2022-06-01&daterec_to%3APARAM=2022-09-30&datedec_from%3APARAM=&datedec_to%3APARAM=&pagerecs=50&orderxyz%3APARAM=REG_DATE_DT%3ADESCENDING&SearchType%3APARAM=Advanced', headers=headers)
 
-addressList = []
-
-binlist = []
-
 soup = BeautifulSoup(r.content, 'lxml')
 
 houselist = soup.find_all('tr')
@@ -21,17 +17,9 @@ a = []
 
 for house in houselist:
     if (house.find('td', string=re.compile('extension'))):
-        a.append(house)
+        linkslist.append(house)
 
 print(a)
-
-# print(houselist)
-# propolsalsList = []
-
-
-# for house in houselist:
-#     for link in house.find_all('td', string=re.compile('extension')):
-#         link.get_text()
 
 
         
